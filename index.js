61c038995a64f1309f4e76e9cd46058a23552a97
sagarpanchani <sagarkmar92@gmail.com>
2022-02-17 16:00:03 +0500

    for-lop-table

---
+++ index.js
@@ -0,0 +1,53 @@
+// TABLE 2
+
+for (var  j = 1; j <= 10; j++){
+		console.log(j+"×" +2 +"=" + j*2)
+		
+		}
+
+// TABLE 3
+for (var  j = 1; j <= 10; j++){
+	console.log(j+"×" +3 +"=" + j*3)
+	
+	}
+
+	// TABLE 4
+for (var  j = 1; j <= 10; j++){
+	console.log(j+ "×" +4 +"=" + j*4)
+	
+	}
+
+		// TABLE 5
+for (var  j = 1; j <= 10; j++){
+	console.log(j+"×" +5 +"=" + j*5)
+	
+	}
+
+			// TABLE 6
+for (var  j = 1; j <= 10; j++){
+	console.log(j+"×" +6 +"=" + j*6)
+	
+	}
+
+			// TABLE 7
+for (var  j = 1; j <= 10; j++){
+	console.log(j+"×" +7 +"=" + j*7)
+	
+	}
+
+			// TABLE 8
+for (var  j = 1; j <= 10; j++){
+	console.log(j+"×" +8 +"=" + j*8)
+	
+	}
+				// TABLE 9
+for (var  j = 1; j <= 10; j++){
+	console.log(j+"×" +9 +"=" + j*9)
+	
+	}
+
+				// TABLE 10
+for (var  j = 1; j <= 10; j++){
+	console.log(j+"×" +10 +"=" + j*10)
+	
+	}
